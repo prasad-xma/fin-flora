7e94b484f9af93b8e756ed41c6a91a35728d5799
Prasad Manamperi <prasadmanamperi130@gmail.com>
2025-12-09 12:33:30 +0530

    create Dockerfile.php

---
+++ Dockerfile.php
@@ -0,0 +1,15 @@
+FROM php:8.2-fpm
+
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    git unzip curl libzip-dev \
+    && docker-php-ext-install pdo pdo_mysql zip
+
+# Install Composer
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+WORKDIR /var/www
+
+COPY . .
+
+RUN composer install
